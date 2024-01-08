0370e3bc4642c3aaf4096d9d594797e62ee66ed8
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-01-08 01:44:54 -0300

    :sparkles: feat: add movie details component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,31 @@
 "use client";
+import { useState } from "react";
 import MoviesList from "./components/movies-list";
 import MoviesWatched from "./components/movies-watched";
 import { useMoviesContext } from "./contexts/movies-context";
+import MovieDetails from "./components/movie-details";
+
+export interface MovieOpenTypes {
+  isOpen: boolean;
+  id?: number;
+}
 
 export default function Home() {
   const { movies } = useMoviesContext();
-  console.log(movies);
+  const [isMovieInfoOpen, setIsMovieInfoOpen] = useState<MovieOpenTypes>({
+    isOpen: false,
+  });
   return (
     <div className="flex  w-full flex-col gap-4 p-2 md:grid md:grid-cols-2 ">
-      <MoviesWatched />
-      <MoviesList movies={movies} />
+      <MoviesWatched setIsMovieInfoOpen={setIsMovieInfoOpen} />
+      {isMovieInfoOpen.isOpen ? (
+        <MovieDetails
+          movieId={isMovieInfoOpen.id}
+          setIsMovieInfoOpen={setIsMovieInfoOpen}
+        />
+      ) : (
+        <MoviesList setIsMovieInfoOpen={setIsMovieInfoOpen} movies={movies} />
+      )}
     </div>
   );
 }
