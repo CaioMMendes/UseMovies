41e8b25ffaef3f633ce8dc5d0d3072dae03881fd
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-01-09 20:37:46 -0300

    :bug: fix: src image fallback

--- src/app/components/image-with-fallback.tsx
+++ src/app/components/image-with-fallback.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image, { ImageProps } from "next/image";
 
 interface ImageLoaderProps extends Pick<ImageProps, "loader"> {}
@@ -17,29 +17,27 @@ interface ImageWithFallbackProps extends ImageProps {
   fallback?: string;
 }
 
-const ImageWithFallback = ({
-  fallback = "image-not-found-poster.png",
-  ...props
-}: ImageWithFallbackProps) => {
-  const { src, ...rest } = props;
-  const [imgSrc, setImgSrc] = useState(src);
-  const imageLoader: ImageLoaderFunction = ({ src, width = 300, quality }) => {
-    console.log(src);
-    return `https://example.com/${src}?w=${width}&q=${quality || 75}`;
-  };
+const ImageWithFallback = React.forwardRef<
+  HTMLImageElement,
+  ImageWithFallbackProps
+>(({ fallback = "/image-not-found-poster.png", src, alt, ...props }, ref) => {
+  const [error, setError] = useState<boolean>(false);
+  // const imageLoader: ImageLoaderFunction = ({ src, width = 300, quality }) => {
+  //   console.log(src);
+  //   return `https://example.com/${src}?w=${width}&q=${quality || 75}`;
+  // };
 
   return (
     <Image
-      {...rest}
-      alt={rest.alt}
-      src={imgSrc}
+      {...props}
+      src={error ? fallback : src}
+      alt={alt}
+      ref={ref}
       // loader={imageLoader}
 
-      onError={() => {
-        setImgSrc(fallback);
-      }}
+      onError={() => setError(true)}
     />
   );
-};
-
+});
+ImageWithFallback.displayName = "ImageWithFallback";
 export default ImageWithFallback;
