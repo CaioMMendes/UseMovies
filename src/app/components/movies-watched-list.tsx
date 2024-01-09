405f809abf5c3f505e3dccaa3c833f8d8ae98e1c
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-01-09 15:44:43 -0300

    :sparkles: feat: Add localstorage to watched movies

--- src/app/components/movies-watched-list.tsx
+++ src/app/components/movies-watched-list.tsx
@@ -1,35 +1,64 @@
-import { Movie } from "../contexts/movies-context";
+"use client";
+import { useEffect, useState } from "react";
+import {
+  Movie,
+  WatchedMoviesTypes,
+  useMoviesContext,
+} from "../contexts/movies-context";
 import { MovieOpenTypes } from "../page";
 import MovieItem from "./movie-item";
+import MoviesWatched from "./movies-watched";
 
 interface MoiviesWatchedListProps {
-  movies: Movie[];
   setIsMovieInfoOpen: React.Dispatch<React.SetStateAction<MovieOpenTypes>>;
 }
 
-const MoviesWatchedList = ({
-  movies,
-  setIsMovieInfoOpen,
-}: MoiviesWatchedListProps) => {
+const MoviesWatchedList = ({ setIsMovieInfoOpen }: MoiviesWatchedListProps) => {
+  const { watchedMovies, setWatchedMovies } = useMoviesContext();
+
+  useEffect(() => {
+    const storageMovies = localStorage.getItem("movies")!;
+    if (Array.isArray(JSON.parse(storageMovies))) {
+      setWatchedMovies(JSON.parse(storageMovies));
+    } else {
+      localStorage.setItem("movies", JSON.stringify([]));
+    }
+    //eslint-disable-next-line
+  }, []);
+
   return (
     <div className="flex w-full flex-col items-start justify-start gap-2 rounded-lg bg-primary-3  ">
-      <div className={`flex w-full flex-col rounded-b-lg bg-primary-3  p-2`}>
-        <p className="flex items-center gap-1 text-lg">
-          Você possuí <strong> X </strong> filmes assistidos.
-        </p>
+      <div
+        className={`flex w-full flex-col rounded-b-lg bg-primary-3  px-2 pt-2 ${
+          watchedMovies.length === 0 && "pb-2"
+        }`}
+      >
+        {watchedMovies.length === 0 ? (
+          <p className="flex items-center gap-1 text-lg">
+            Comece adicionando um filme a lista.
+          </p>
+        ) : (
+          <p className="flex items-center gap-1 text-lg">
+            Você possui <strong> {watchedMovies.length} </strong> filme
+            {watchedMovies.length > 1 && "s"} assistido
+            {watchedMovies.length > 1 && "s"}.
+          </p>
+        )}
       </div>
-      <div className="flex w-full flex-col gap-2 p-2">
-        {movies.map((movie) => {
-          return (
-            <MovieItem
-              movieId={movie.id}
-              setIsMovieInfoOpen={setIsMovieInfoOpen}
-              key={movie.id}
-              movie={movie}
-            />
-          );
-        })}
-      </div>
+      {watchedMovies.length > 0 && (
+        <ul className="flex w-full flex-col gap-2 ">
+          {watchedMovies.map((movie: WatchedMoviesTypes) => {
+            return (
+              <MovieItem
+                movieId={movie.id}
+                setIsMovieInfoOpen={setIsMovieInfoOpen}
+                key={movie.id}
+                movie={movie}
+              />
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
